d6fc0e50eddad4b313e29a18ea3d8d6d5bd39a5b
Paulo Gualter <paulogualter@gmail.com>
2024-02-01 21:55:31 -0300

    Update urlsDomain.sh

--- urlsDomain.sh
+++ urlsDomain.sh
@@ -18,7 +18,7 @@ echo '
 ____________________________________[++++++++]____________________________________
 ' | lolcat          
 
-# Define some colors
+# Define cores
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
@@ -26,7 +26,7 @@ NC='\033[0m' # No Color
 # Defina o domínio alvo
 dominio=$1
 
-# Verifique se o domínio foi fornecido
+# Verifica se o domínio foi fornecido
 if [ -z "$dominio" ]
 then
     echo -e "Por favor, forneça um domínio como argumento. Sem http://www ou https://"
@@ -36,45 +36,11 @@ fi
 
 $dev = 2>/dev/null
 
-echo "... [+] Verificando Dependencias ... [+]"
 
-echo
-
-# Verificar se subfinder está instalado
-if ! command -v subfinder &> /dev/null
-then
-    sudo apt-get install subfinder
-else
-    echo " "
-fi
-
-
-# Verificar se subfinder está instalado
-if ! command -v sublist3r &> /dev/null
-then
-    sudo apt-get install sublist3r
-else
-    echo " "
-fi
-
-
-# Verificar se httpx está instalado
-if ! command -v httpx &> /dev/null
-then
-    sudo apt-get install httpx
-else
-sleep 3
-echo "... [+] Dependencias Verificadas ... [+]"
-    echo -e "....................................................................\n "
-fi
-
-
-
-
-
-# Use subfinder e assetfinder para encontrar subdomínios e salve os resultados em um arquivo
+# Busca por Subdominios
 echo -e "Busca de subdominios sendo realizado e validado no dominio https://www."$dominio "\n"
 sleep 2
+echo - "[+] Processo em adanmento [+]"
 
 subfinder -d $dominio -silent $dev | anew | tee subs.txt
 assetfinder --subs-only $dominio $dev | anew >> subs.txt
@@ -82,16 +48,19 @@ sublist3r -d $dominio -e baidu,yahoo,google,bing,ask,netcraft,dnsdumpster,threat
 
 
 
-# Use httpx para verificar quais subdomínios têm um servidor HTTP ativo
+# Verifica se os subdominios sao validos
 cat subs.txt | httpx -silent -server -ip -sc > resultadosIPs.txt
 echo -e "\n"
 echo -e "${GREEN}Subdominios Unicos e Validados - ABAIXO ${NC}"
 echo -e "---------------------------------------\n"
+
+#Grava em arquivo os subdominios
 cat resultadosIPs.txt
 echo -e "\n"
+
+#Verifica a quantidade de subdominios validos
 qtd=$(cat resultadosIPs.txt | wc -l)
-echo -e "Total de Subdominios Validos= ${qtd}" 
+echo -e "Total de Subdominios Validos = ${qtd}" 
 echo -e "\n"
 echo "Scan em https://www.${dominio} concluido com Exito"
 echo -e "${GREEN}[+] Finalizado [+]${NC}"
-
